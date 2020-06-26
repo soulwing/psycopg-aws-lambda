a5c6a8eca0a21c7e80668f09d212f4f192e77282
Carl Harris <ceharris@vt.edu>
2020-06-26 12:57:56 -0400

    install pip and setuptools

--- bin/install-pkgs.sh
+++ bin/install-pkgs.sh
@@ -4,5 +4,5 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y python${PY_VER}-dev
-sudo python3.8 -m pip install --upgrade pip setuptools
+sudo apt-get install -y python${PY_VER}-dev python3-pip
+sudo -H pip3 install --upgrade pip setuptools
