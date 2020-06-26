0d73545bdf2313f91d146aa43f40803910156242
Carl Harris <ceharris@vt.edu>
2020-06-26 11:17:26 -0400

    WIP on build script

---
+++ bin/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+PG_DIR=postgresql-${PG_VER}
+PSY_DIR=psycopg2-${PSY_VER}
+PG_PREFIX=$(PWD)
+
+pushd $PG_DIR
+./configure --prefix=$PG_PREFIX --without-readline --without-zlib
+make all install
+popd
+
+pushd $PSY_DIR
+popd
--- bin/fetch-deps.sh
+++ bin/fetch-deps.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-PG_VER=10.9
-PSY_VER=2_8_5
 PG_SRC=https://ftp.postgresql.org/pub/source/v${PG_VER}/postgresql-${PG_VER}.tar.gz
 PSY_SRC=https://github.com/psycopg/psycopg2/archive/${PSY_VER}.tar.gz
 
 wget -O postgresql.tar.gz $PG_SRC
 wget -O psycopg2.tar.gz $PSY_SRC
+tar -xvf postgresql.tar.gz
+tar -xvf psycopg2.tar.gz
+rm postgresql.tar.gz psycopg2.tar.gz
