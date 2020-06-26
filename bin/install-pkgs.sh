a6f0980df95eb189bf8d461eec6c28a33d56ff40
Carl Harris <ceharris@vt.edu>
2020-06-26 15:28:31 -0400

    install pip using sudo

--- bin/install-pkgs.sh
+++ bin/install-pkgs.sh
@@ -5,4 +5,4 @@ sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get install -y python${PY_VER}-dev
 wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
-python3.8 get-pip.py
+sudo -H python3.8 $PWD/get-pip.py
