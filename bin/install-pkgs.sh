aa9c641f608374b835ebdceafe92c00c28dd7774
Carl Harris <ceharris@vt.edu>
2020-06-26 12:32:18 -0400

    specify python version in config

--- bin/install-pkgs.sh
+++ bin/install-pkgs.sh
@@ -4,4 +4,4 @@ sudo apt-get update
 sudo apt-get install -y software-properties-common
 
 sudo add-apt-repository -y ppa:deadsnakes/ppa
-sudo apt-get install -y python3.8
+sudo apt-get install -y python${PY_VER}
