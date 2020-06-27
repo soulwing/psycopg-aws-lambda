827e25bc2729df2c83e7c00b6205f01fd96c5342
Carl Harris <ceharris@vt.edu>
2020-06-27 05:29:18 -0400

    improve wget options; add comments

--- bin/fetch-deps.sh
+++ bin/fetch-deps.sh
@@ -3,8 +3,12 @@
 PG_SRC=https://ftp.postgresql.org/pub/source/v${PG_VER}/postgresql-${PG_VER}.tar.gz
 PSY_SRC=https://github.com/psycopg/psycopg2/archive/${PSY_VER}.tar.gz
 
-wget -O postgresql.tar.gz $PG_SRC
-wget -O psycopg2.tar.gz $PSY_SRC
+# fetch and unpack postgresql source
+wget --quiet --output-document postgresql.tar.gz $PG_SRC
 tar -xf postgresql.tar.gz
+rm postgresql.tar.gz
+
+# fetch and unpack psycopg2 source
+wget --quiet --output-document psycopg2.tar.gz $PSY_SRC
 tar -xf psycopg2.tar.gz
-rm postgresql.tar.gz psycopg2.tar.gz
+rm psycopg2.tar.gz
