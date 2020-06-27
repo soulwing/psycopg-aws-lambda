9040d86c451e43a9e89d2918530cb037f00059b7
Carl Harris <ceharris@vt.edu>
2020-06-27 05:27:04 -0400

    improve wget options; add comments

--- bin/install-pkgs.sh
+++ bin/install-pkgs.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Install Python version of our choice
 sudo apt-get update
 sudo apt-get install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt-get install -y python${PY_VER}-dev
-wget -O get-pip.py https://bootstrap.pypa.io/get-pip.py
+
+# Install PIP using the Python version we installed
+wget --quiet --output-document get-pip.py https://bootstrap.pypa.io/get-pip.py
 sudo -H python${PY_VER} $PWD/get-pip.py
